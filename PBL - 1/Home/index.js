91c8a4004abd7c45537a1cc245e8de900267a44c
tanush-shah-6 <tanush.shah.btech2022@sitpune.edu.in>
2024-05-01 20:02:36 +0530

    Update index.js
    updated index.js

--- PBL - 1/Home/index.js
+++ PBL - 1/Home/index.js
@@ -2,25 +2,73 @@
 var durationValue = document.getElementById("durationValue");
 var durationRange = document.getElementById("durationRange");
 
-
 durationValue.innerHTML = durationRange.value; // Display the default slider value
-
+    
 // Update the current slider value (each time you drag the slider handle)
 durationRange.oninput = function() {
     durationValue.innerHTML = this.value;
 }
 
- // JavaScript to display the selected value of the range slider
- var priceValue = document.getElementById("priceValue");
- var priceRange = document.getElementById("priceRange");
- 
+// JavaScript to display the selected value of the range slider
+var priceValue = document.getElementById("priceValue");
+var priceRange = document.getElementById("priceRange");
 
- priceValue.innerHTML = priceRange.value; // Display the default slider value
+priceValue.innerHTML = priceRange.value; // Display the default slider value
 
- // Update the current slider value (each time you drag the slider handle)
- priceRange.oninput = function() {
-     priceValue.innerHTML = this.value;
- }
+// Update the current slider value (each time you drag the slider handle)
+priceRange.oninput = function() {
+    priceValue.innerHTML = this.value;
+}
+
+
+var places = ["Gateway of India", "Juhu", "Raigad Fort", "Shirdi", "Sinhagad Fort"];
+$("#searchResults").removeClass("p-2");
+  
+$(".form-control").on("input", function() {
+    var query = $(this).val().toLowerCase();
+    if (query !== '') {
+    var filteredOptions = places.filter(function(option) {
+        return option.toLowerCase().includes(query);
+    });
+
+    displayResults(filteredOptions);
+    } else {
+    $("#searchResults").empty();
+    }
+});
+
+function displayResults(results) {
+    var resultList = $("#searchResults");
+    resultList.empty();
+    resultList.hide();
+    $.each(results, function(index, result) {
+    resultList.append("<li>" + result + "</li>");
+    });
+    $("#searchResults").addClass("p-2");
+    resultList.slideDown();
+
+}
+
+$(document).on('click', '#searchResults li', function() {
+    $(".form-control").val($(this).text());
+    $("#searchResults").empty();
+    $("#searchResults").hide();
+    $("#searchResults").removeClass("p-2"); 
+    
+});
+  
+    
+$(".input-group-text").click(function()
+{
+    var destination = $(".form-control").val();
+    destinationLength = destination.length;
+    var firstWord = destination.split(" ");
+    if(destinationLength !== 0)
+    {
+        window.location.href = "../"+destination+"/"+firstWord[0].toLowerCase()+".html";
+    }
+});
+
 
 $(".preferences").hide();
 $(".or button").click(function()
